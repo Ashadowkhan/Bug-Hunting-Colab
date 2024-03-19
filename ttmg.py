e715d3ba3b3ca350778f9cf7cb81678b5c3cbf55
Ashadowkhan <89469016+Ashadowkhan@users.noreply.github.com>
2024-03-20 00:22:37 +0600

    Update ttmg.py

--- ttmg.py
+++ ttmg.py
@@ -193,7 +193,7 @@ def _download(url, path):
         raise
 
 def argoTunnel():
-    _download("https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.tgz", "cloudflared.tgz")
+    _download("https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz", "cloudflared.tgz")
     shutil.unpack_archive("cloudflared.tgz")
     cfd_proc = subprocess.Popen(
         ["./cloudflared", "tunnel", "--url", "ssh://localhost:22", "--logfile", "cloudflared.log", "--metrics", "localhost:49589"],
